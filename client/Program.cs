a1c47d5b3055e372f8d39dbb89e346bbb533f1a8
dzobamain <knopkayt26@gmail.com>
2025-06-08 14:07:42 +0200

    Added client

---
+++ client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class CurrencyClient
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Enter two currencies (e.g. USD/EUR) or 'q' to quit: ");
+            string request = Console.ReadLine();
+
+            if (request.ToLower() == "q") break;
+
+            TcpClient client = new TcpClient("127.0.0.1", 5050);
+            NetworkStream stream = client.GetStream();
+
+            byte[] data = Encoding.UTF8.GetBytes(request);
+            stream.Write(data, 0, data.Length);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine($"Response: {response}");
+            client.Close();
+        }
+    }
+}
